cc0c4392268c48a17f70956f049fc8911699823e
Jeff Lynch <jlynch3@uw.edu>
2021-08-29 13:09:52 -0700

    first commit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -78,5 +78,15 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login',{sign_up: true});
+
+});
 
 module.exports = router;
