aca2b81f6972702219a361e29b6a796fbf89918f
Jeff Lynch <jlynch3@uw.edu>
2021-08-31 16:55:27 -0700

    first commit

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const User = require('./User.js');
+const Post = require('./Post.js');
+const Comment = require('./Comment.js');
 
 //grouping Models
 User.hasMany(Post, {
