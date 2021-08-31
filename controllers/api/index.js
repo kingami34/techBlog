98a515db60012cc0b5520268bed4ff96c7e7a95b
Jeff Lynch <jlynch3@uw.edu>
2021-08-31 11:53:05 -0700

    first commit

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
 
-router.use('./Users', userRoutes);
+router.use('./users', userRoutes);
 router.use('/Projects', projectRoutes);
 
 module.exports = router;
